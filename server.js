79be4b0a8bc1d339db7dfc00d222d56d8277fd24
Gooti Gangadhara <68936937+Gangadhar-gdvs@users.noreply.github.com>
2024-09-14 19:02:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const User = require('./models/admin-request'); 
+const User = require('./admin-request'); 
 
 const app = express();
 
