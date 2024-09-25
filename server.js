c4834eef07f4f9f6cd21454531f0bdcac76fe89e
Gooti Gangadhara <68936937+Gangadhar-gdvs@users.noreply.github.com>
2024-09-25 22:06:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,21 +7,23 @@ const adminRoutes = require('./routes/adminRoutes');
 const app = express();
 
 
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); 
 
 
 connectDB();
 
 
-app.use('/api/vendors', vendorRoutes);
-app.use('/api/admin', adminRoutes);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
 
-app.use('/uploads', express.static('uploads'));
+app.use('/api/vendors', vendorRoutes); 
+app.use('/api/admin', adminRoutes); 
 
 
 const PORT = process.env.PORT || 5000;
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
